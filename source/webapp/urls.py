377e0f85207ddc4b73682185a953d8b4e37cab3a
galubezzz galubezzz <galubezzzgalubezzz@galubezzzs-MacBook-Air.local>
2019-01-12 00:25:43 -0800

    added post create

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from webapp.views import PostDetailView, PostListView
+from webapp.views import PostDetailView, PostListView, PostCreateView
 
 app_name = 'webapp'
 
 urlpatterns = [
-    path('post/<int:pk>', PostDetailView.as_view(), name='post_detail'),
-    path('', PostListView.as_view(), name='posts')
+    path('post/<int:pk>', PostDetailView.as_view(), name='post_details'),
+    path('', PostListView.as_view(), name='posts'),
+    path('post/create', PostCreateView.as_view(), name='post_create')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from webapp.models import User, UserInfo, Post
 from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from webapp.forms import PostForm
 
 class PostDetailView(DetailView):
     model = Post
@@ -11,3 +13,11 @@ class PostDetailView(DetailView):
 class PostListView(ListView):
     model = Post
     template_name = 'post_list.html'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    form_class = PostForm
+
+    def get_success_url(self):
+        return reverse('webapp:post_details', kwargs={'pk': self.object.pk})
