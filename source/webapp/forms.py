3e65dfb3683856121f3f92e1f6d344fdefc32392
galubezzz galubezzz <galubezzzgalubezzz@galubezzzs-MacBook-Air.local>
2019-01-12 05:58:58 -0800

    minor templates changes

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -5,7 +5,7 @@ from webapp.models import User, UserInfo, Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        exclude =['author']
+        exclude =['author', 'date']
 
 class UserForm(forms.ModelForm):
     class Meta:
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -36,7 +36,6 @@ class PostUpdateView(LoginRequiredMixin, UpdateView):
     model = Post
     template_name = 'post_update.html'
     form_class = PostForm
-    success_url = reverse_lazy('webapp:posts')
 
     def get(self, request, pk):
         post = get_object_or_404(Post, pk=pk)
@@ -44,6 +43,8 @@ class PostUpdateView(LoginRequiredMixin, UpdateView):
             return HttpResponseRedirect(reverse('webapp:post_details', kwargs={'pk': pk}))
         return super().get(request, pk=pk)
 
+    def get_success_url(self):
+        return reverse('webapp:post_details', kwargs={'pk': self.object.pk})
 
 class PostDeleteView(DeleteView):
     model = Post
@@ -79,7 +80,7 @@ class UserUpdateView(UpdateView):
     form_class = UserForm
 
     def get_success_url(self):
-        return reverse('webapp:user_detail', kwargs={'pk': self.object.pk})
+        return reverse('webapp:user_details', kwargs={'pk': self.object.pk})
 
     def get(self, request, pk):
         user = get_object_or_404(Post, pk=pk)
