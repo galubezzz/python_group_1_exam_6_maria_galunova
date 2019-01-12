da98147ac2f1aac05409a73c38f3ed369c153b63
galubezzz galubezzz <galubezzzgalubezzz@galubezzzs-MacBook-Air.local>
2019-01-11 21:53:31 -0800

    created a project. Added models.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT, related_name='UserInfo', verbose_name="Пользователь")
+    phone = models.CharField(max_length=50, verbose_name="Телефон")
+    photo = models.ImageField(verbose_name='Фотография')
+    friends = models.ManyToManyField(User, blank=True, null=True, related_name="my_friends", verbose_name="Друзья")
+
+    def __str__(self):
+        return self.user.get_full_name()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Заголовок")
+    text = models.TextField(max_length=5000, verbose_name="Текст")
+    date = models.DateTimeField(default=datetime.now, verbose_name='Дата')
+    author = models.ForeignKey(User, on_delete=models.PROTECT, related_name='posts_by_user', verbose_name='Автор')
+
+    def __str__(self):
+        return self.title
+
